a641ef8e56f1f420342d2d40e01826cecf251b02
Jökull Sólberg Auðunsson <jokull@solberg.is>
2021-04-12 12:31:20 +0000

    Seðlabanki moved ÍLS data to a new series, refactor to not rely on .ics file

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import datetime as dt
+import sqlite3
 from io import BytesIO
 from typing import List, Optional, Tuple
-import sqlite3
 
-import ics
+import arrow
 import openpyxl
-from openpyxl.worksheet.worksheet import Worksheet
 import requests
+from openpyxl.worksheet.worksheet import Worksheet
 
-from schemas import schemas, Row
-
+from schemas import Row, Workbook, schemas
 
 DBNAME = "credit.db"
 
@@ -34,78 +33,56 @@ def create_db(path=DBNAME):
     return dbconn
 
 
-SEDLABANKI_ICS_URL = "https://www.sedlabanki.is/library/Fylgiskjol/Hagtolur/Birtingar_2021%20Calendar.ics"
-EVENTS = dict(
-    (name, []) for name in ("Bankakerfi", "Önnur fjármálafyrirtæki", "Lífeyrissjóðir")
-)
+def iter_series(wb_model: Workbook, wb: openpyxl.Workbook):
+    for sheet_model in wb_model.sheets:
+        sheet: Worksheet = wb.worksheets[sheet_model.sheet]  # type: ignore
+        _series: List[Tuple[Row, List[Optional[int]]]] = []
+        month_values = [
+            c.value
+            for c in sheet[sheet_model.dates_row][sheet_model.from_ : sheet.max_column]
+        ]
+
+        for row in sheet_model.rows:
+            cells = sheet[row.row][sheet_model.from_ : sheet.max_column]
+            assert len(month_values) == len(cells)
+            values: List[Optional[int]] = [
+                (int(c.value * 1_000_000) if c.value else None) for c in cells
+            ]
+            _series.append((row, values))
+
+        yield (
+            month_values,
+            _series,
+        )
 
 
-def get_series(date, name):
-    series = []
-    for wb_model in schemas[name]:
-        if wb_model.sheets is None:
-            continue
+def get_latest_workbook(wb_model: Workbook) -> Tuple[openpyxl.Workbook, str]:
+    date = arrow.now()
+    while True:
         url = wb_model.url.format(year=date.year, month=date.month)
         response = requests.get(url)
         if response.status_code == 404:
-            return None
-        wb: openpyxl.Workbook = openpyxl.load_workbook(
-            BytesIO(response.content), data_only=True
-        )
-        for sheet_model in wb_model.sheets:
-            sheet: Worksheet = wb.worksheets[sheet_model.sheet]  # type: ignore
-            _series: List[Tuple[Row, List[Optional[int]]]] = []
-            month_values = [
-                c.value
-                for c in sheet[sheet_model.dates_row][
-                    sheet_model.from_ : sheet.max_column
-                ]
-            ]
-
-            for row in sheet_model.rows:
-                cells = sheet[row.row][sheet_model.from_ : sheet.max_column]
-                assert len(month_values) == len(cells)
-                values: List[Optional[int]] = [
-                    (int(c.value * 1_000_000) if c.value else None) for c in cells
-                ]
-                _series.append((row, values))
-
-            series.append(
-                (
-                    month_values,
-                    _series,
-                )
-            )
-
-    return series
+            date = date.shift(months=-1)
+        else:
+            break
+    wb: openpyxl.Workbook = openpyxl.load_workbook(
+        BytesIO(response.content), data_only=True
+    )
+    return wb, url
 
 
 def main():
-    today = dt.date.today()
-    response = requests.get(SEDLABANKI_ICS_URL)
-    response.encoding = "utf-8"
-    calendar = ics.Calendar(response.text)
-
-    for event in calendar.timeline:
-        if event.begin.date() > today:
-            continue
-        if event.name not in EVENTS:
-            continue
-        EVENTS[event.name].append(event.begin)
 
     dbconn = create_db()
 
-    for name, events in EVENTS.items():
-        latest = events[-1].shift(months=-1)
-        while True:
-            series = get_series(latest, name)
-            if series:
-                break
-            else:
-                latest = latest.shift(months=-1)
+    for wb_model in schemas:
+        if wb_model.sheets is None:
+            continue
+        wb, url = get_latest_workbook(wb_model)
 
-        print(name, "-", latest.humanize())
-        for month_values, s in series:
+        print(url)
+
+        for month_values, s in iter_series(wb_model, wb):
             for row, values in s:
                 for date, value in zip(month_values, values):
                     dbconn.execute(
--- schemas.py
+++ schemas.py
@@ -27,154 +27,151 @@ class Workbook(BaseModel):
     sheets: Optional[List[Sheet]]
 
 
-schemas: Dict[str, Tuple[Workbook, ...]] = {
-    "Bankakerfi": (
-        Workbook.parse_obj(
-            {
-                "url": BASE_URL + "INN_Utlan_{month:02d}{year}.xlsx",
-                "sheets": (
-                    {
-                        "sheet": 5,
-                        "from_": 3,
-                        "dates_row": 9,
-                        "rows": (
-                            {
-                                "institute": "bank",
-                                "sector": "household",
-                                "category": "indexed",
-                                "row": 15,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "household",
-                                "category": "nonindexed",
-                                "row": 18,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "household",
-                                "category": "foreign",
-                                "row": 21,
-                            },
-                        ),
-                    },
-                    {
-                        "sheet": 2,
-                        "from_": 3,
-                        "dates_row": 9,
-                        "rows": (
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "agriculture",
-                                "row": 13,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "fisheries",
-                                "row": 14,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "manufacturing",
-                                "row": 15,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "utilities",
-                                "row": 19,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "construction",
-                                "row": 20,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "retail",
-                                "row": 21,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "transport-and-communications",
-                                "row": 22,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "services",
-                                "row": 23,
-                            },
-                            {
-                                "institute": "bank",
-                                "sector": "business",
-                                "industry": "other",
-                                "row": 27,
-                            },
-                        ),
-                    },
-                ),
-            }
-        ),
+schemas: Tuple[Workbook, ...] = (
+    # Bankakerfi
+    Workbook.parse_obj(
+        {
+            "url": BASE_URL + "INN_Utlan_{month:02d}{year}.xlsx",
+            "sheets": (
+                {
+                    "sheet": 5,
+                    "from_": 3,
+                    "dates_row": 9,
+                    "rows": (
+                        {
+                            "institute": "bank",
+                            "sector": "household",
+                            "category": "indexed",
+                            "row": 15,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "household",
+                            "category": "nonindexed",
+                            "row": 18,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "household",
+                            "category": "foreign",
+                            "row": 21,
+                        },
+                    ),
+                },
+                {
+                    "sheet": 2,
+                    "from_": 3,
+                    "dates_row": 9,
+                    "rows": (
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "agriculture",
+                            "row": 13,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "fisheries",
+                            "row": 14,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "manufacturing",
+                            "row": 15,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "utilities",
+                            "row": 19,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "construction",
+                            "row": 20,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "retail",
+                            "row": 21,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "transport-and-communications",
+                            "row": 22,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "services",
+                            "row": 23,
+                        },
+                        {
+                            "institute": "bank",
+                            "sector": "business",
+                            "industry": "other",
+                            "row": 27,
+                        },
+                    ),
+                },
+            ),
+        }
     ),
-    "Önnur fjármálafyrirtæki": (
-        Workbook.parse_obj(
-            {
-                "url": BASE_URL + "YFT_Utlan%20e%20geirum_{year}M{month:02d}.xlsx",
-                "sheets": (
-                    {
-                        "from_": 2,
-                        "dates_row": 9,
-                        "rows": (
-                            {
-                                "institute": "other",
-                                "sector": "household",
-                                "category": "indexed",
-                                "row": 27,
-                            },
-                        ),
-                    },
-                ),
-            }
-        ),
-        Workbook.parse_obj(
-            {"url": BASE_URL + "YFT_Utlan%20e%20tegund_{year}M{month:02d}.xlsx"}
-        ),
+    # Lánasjóðir ríkisins
+    Workbook.parse_obj(
+        {
+            "url": BASE_URL + "LSJ_Utlan%20e%20geirum_{year}M{month}.xlsx",
+            "sheets": (
+                {
+                    "from_": 2,
+                    "dates_row": 9,
+                    "rows": (
+                        {
+                            "institute": "other",
+                            "sector": "household",
+                            "category": "indexed",
+                            "row": 27,
+                        },
+                    ),
+                },
+            ),
+        }
     ),
-    "Lífeyrissjóðir": (
-        Workbook.parse_obj(
-            {
-                "url": BASE_URL + "LIF_Utlan%20e%20geirum_{year}M{month}.xlsx",
-                "sheets": (
-                    {
-                        "from_": 2,
-                        "dates_row": 9,
-                        "rows": (
-                            {
-                                "institute": "pension",
-                                "sector": "household",
-                                "category": "indexed",
-                                "row": 27,
-                            },
-                            {
-                                "institute": "pension",
-                                "sector": "household",
-                                "category": "nonindexed",
-                                "row": 28,
-                            },
-                        ),
-                    },
-                ),
-            },
-        ),
-        Workbook.parse_obj(
-            {"url": BASE_URL + "LIF_Utlan%20e%20tegund_{year}M{month}.xlsx"}
-        ),
+    Workbook.parse_obj(
+        {"url": BASE_URL + "LSJ_Utlan%20e%20tegund_{year}M{month}.xlsx"}
     ),
-}
+    # Lífeyrissjóðir
+    Workbook.parse_obj(
+        {
+            "url": BASE_URL + "LIF_Utlan%20e%20geirum_{year}M{month}.xlsx",
+            "sheets": (
+                {
+                    "from_": 2,
+                    "dates_row": 9,
+                    "rows": (
+                        {
+                            "institute": "pension",
+                            "sector": "household",
+                            "category": "indexed",
+                            "row": 27,
+                        },
+                        {
+                            "institute": "pension",
+                            "sector": "household",
+                            "category": "nonindexed",
+                            "row": 28,
+                        },
+                    ),
+                },
+            ),
+        },
+    ),
+    Workbook.parse_obj(
+        {"url": BASE_URL + "LIF_Utlan%20e%20tegund_{year}M{month}.xlsx"}
+    ),
+)
